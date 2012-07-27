cc1e743c58abdaa384684980105af81fd9d620e8
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-07-27 16:53:16 +0900

    add commit.

---
+++ FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"495169d4c351cef1b5fa5d2e664925d0"
