626fc324a133e8c6696399601e8f88626a05f0f7
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-07-27 20:40:16 +0900

    landscape support #9

--- FlickrFetcher/TopPlacesTableViewController.m
+++ FlickrFetcher/TopPlacesTableViewController.m
@@ -85,7 +85,8 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+//    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 #pragma mark - Table view data source
